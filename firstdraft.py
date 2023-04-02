9d21a42ef095dbf97be3ab724694cc6d244884d9
KayimSaidB <kayim94@LAPTOP-CN64OR2N>
2023-04-02 18:31:54 +0200

    added class for artist

--- firstdraft.py
+++ firstdraft.py
@@ -16,14 +16,22 @@ artists = spotify.artist_related_artists(tyler_uri)
 
 
 @dataclass
+class Artist:
+    name: str
+    uri: str
+    id: str
+
+
+@dataclass
 class Track:
     name: str
     external_urls: str
-    artists: list[str]
+    artists: list[Artist]
     explicit: bool
     duration_ms: int
-    album: str
+    # album: str
     uri: str
+    id: str
 
 
 def get_tracks_rec_from_artist(
@@ -32,14 +40,36 @@ def get_tracks_rec_from_artist(
     recs = spotify.recommendations(seed_artists=[artist_uri], limit=limit)
     raw_tracks = recs["tracks"]
     tracks = []
-    for track in raw_tracks:
-        track_obj = Track(**{k: v for k, v in track.items() if k in fields(Track)})
-        tracks.append(track_obj)
-
+    for track_dict in raw_tracks:
+        tracks.append(create_track_from_dict(track_dict))
     return tracks
 
 
+def create_track_from_dict(track_dict: dict) -> Track:
+    print(track_dict["album"])
+    dict_k = {
+        k: v
+        for k, v in track_dict.items()
+        if k in [field.name for field in fields(Track)]
+    }
+    artists = []
+    for artist in dict_k["artists"]:
+        dict_artist = {
+            k: v
+            for k, v in artist.items()
+            if k in [field.name for field in fields(Artist)]
+        }
+        artist_obj = Artist(**dict_artist)
+        artists.append(artist_obj)
+    dict_k["artists"] = artists
+    track_obj = Track(**dict_k)
+    return track_obj
+
+
+# TODO: factorize the creation
+
 tracks = get_tracks_rec_from_artist(tyler_uri, spotify, limit=5)
+# print(tracks)
 
 
 def get_artists_related_artists(artist_uri, spotify):
