8d755ec9dea379dcdc741c9ed5862565c70f0286
KayimSaidB <kayim94@LAPTOP-CN64OR2N>
2023-04-09 16:59:22 +0200

    clean classes and add audio features

--- firstdraft.py
+++ firstdraft.py
@@ -1,6 +1,6 @@
 from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy import Spotify
-from dataclasses import dataclass, fields
+from dataclasses import dataclass
 
 tyler_uri = "spotify:artist:4V8LLVI7PbaPR0K2TGSxFF"
 spotify = Spotify(client_credentials_manager=SpotifyClientCredentials())
@@ -15,11 +15,38 @@ while results["next"]:
 artists = spotify.artist_related_artists(tyler_uri)
 
 
+def create_dict_for_object(dataclass, dict):
+    dict_k = {
+        k: v
+        for k, v in dict.items()
+        if k in [field for field in dataclass.__annotations__]
+    }
+    return dict_k
+
+
 @dataclass
 class Artist:
     name: str
     uri: str
     id: str
+
+
+@dataclass
+class AudioFeatures:
+    danceability: float
+    energy: float
+    key: int
+    loudness: float
+    mode: int
+    speechiness: float
+    acousticness: float
+    instrumentalness: float
+    liveness: float
+    valence: float
+    tempo: float
+    type: str
+    duration_ms: int
+    time_signature: int
 
 
 @dataclass
@@ -29,9 +56,10 @@ class Track:
     artists: list[Artist]
     explicit: bool
     duration_ms: int
-    # album: str
+    album: str
     uri: str
     id: str
+    features: AudioFeatures
 
 
 def get_tracks_rec_from_artist(
@@ -45,31 +73,32 @@ def get_tracks_rec_from_artist(
     return tracks
 
 
+def get_audio_features_from_track(track_uri: str, spotify: Spotify) -> dict:
+    features = spotify.audio_features(track_uri)
+    return features[0]
+
+
 def create_track_from_dict(track_dict: dict) -> Track:
-    print(track_dict["album"])
-    dict_k = {
-        k: v
-        for k, v in track_dict.items()
-        if k in [field.name for field in fields(Track)]
-    }
+    dict_k = create_dict_for_object(Track, track_dict)
     artists = []
-    for artist in dict_k["artists"]:
-        dict_artist = {
-            k: v
-            for k, v in artist.items()
-            if k in [field.name for field in fields(Artist)]
-        }
-        artist_obj = Artist(**dict_artist)
+    for artist_dict in dict_k["artists"]:
+        artist_obj = Artist(**create_dict_for_object(Artist, artist_dict))
         artists.append(artist_obj)
+    audio_features_dict = get_audio_features_from_track(dict_k["uri"], spotify)
+    audio_features_obj = AudioFeatures(
+        **create_dict_for_object(AudioFeatures, audio_features_dict)
+    )
+    dict_k["features"] = audio_features_obj
+
     dict_k["artists"] = artists
+    dict_k["album"] = track_dict["album"]["name"]
     track_obj = Track(**dict_k)
     return track_obj
 
 
-# TODO: factorize the creation
-
 tracks = get_tracks_rec_from_artist(tyler_uri, spotify, limit=5)
-# print(tracks)
+ids = [track.id for track in tracks]
+print(tracks[0])
 
 
 def get_artists_related_artists(artist_uri, spotify):
